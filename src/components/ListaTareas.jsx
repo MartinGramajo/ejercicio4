1c1f3a651b0c9c73d3a0891a8cb765501af201e2
¨mago¨ <martingramajo08@gmail.com>
2023-07-20 18:43:28 -0300

    En ListaTareas recibimos el props listaDeTareas y lo mapeamos

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,10 +1,12 @@
 import { ListGroup } from "react-bootstrap";
 import ItemTarea from "./ItemTarea";
 
-const ListaTareas = () => {
+const ListaTareas = ({ listaDeTareas }) => {
   return (
     <ListGroup>
-      <ItemTarea />
+      {listaDeTareas.map((tarea, index) => (
+        <ItemTarea tarea={tarea} key={index} />
+      ))}
     </ListGroup>
   );
 };
