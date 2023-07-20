9d69b6f80b34c66b696dbc626dc4782c8f33a738
¨mago¨ <martingramajo08@gmail.com>
2023-07-20 18:43:47 -0300

    en ItemTarea recibimos la tarea por props y la usamos para mostrarla por pantalla

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -2,11 +2,11 @@ import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, ListGroup } from "react-bootstrap";
 
-const ItemTarea = () => {
+const ItemTarea = ({ tarea }) => {
   return (
     <>
       <ListGroup.Item className="d-flex justify-content-between">
-        tarea 1
+        {tarea}
         <Button variant="danger">
           <FontAwesomeIcon className="fs-4" icon={faTrashAlt} />
         </Button>
