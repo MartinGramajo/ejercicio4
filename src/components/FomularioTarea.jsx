c0842684cb277718642c6270450f62416c6146f6
¨mago¨ <martingramajo08@gmail.com>
2023-07-20 18:38:33 -0300

    agrego en FormularioTarea los state y la funcionalidad para capturar el value del input de mi form

--- src/components/FomularioTarea.jsx
+++ src/components/FomularioTarea.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import { Button, Container, Form, InputGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import ListaTareas from "./ListaTareas";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [listaTareas, setListaTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setListaTareas([...listaTareas, tarea]);
+    setTarea("");
+  };
+
+  const handleChange = (e) => {
+    setTarea(e.target.value);
+  };
+
   return (
     <>
       <Container>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <InputGroup className="mb-3">
-            <Form.Control placeholder="Ingrese una Tarea" type="text" />
+            <Form.Control
+              placeholder="Ingrese una Tarea"
+              value={tarea}
+              onChange={handleChange}
+              type="text"
+            />
             <Button
               className="btn-agregar-tarea"
               variant="outline-primary"
